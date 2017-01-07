13b765731347df81ff83cdb790006ce04551bdf6
llathanh1108 <thanh1108@gmail.com>
2017-01-07 10:37:52 +0700

    Init
    full

---
+++ HoaBanTravel/HoaBanTravel/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoaBanTravel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Dulichtrongnuoc()
+        {
+            ViewBag.Message = "Trang du lịch trong nước";
+
+            return View();
+        }
+        public ActionResult Dulichnuocngoai()
+        {
+            ViewBag.Message = "Trang du lịch nước ngoài";
+
+            return View();
+        }
+        public ActionResult Dichvudulich()
+        {
+            ViewBag.Message = "Trang dịch vụ du lịch";
+
+            return View();
+        }
+        public ActionResult Dulichmienbac()
+        {
+            ViewBag.Message = "Trang du lịch miền bắc";
+
+            return View();
+        }
+
+    }
+}
---
+++ HoaBanTravel/HoaBanTravel/Scripts/Front/myScripts.js
@@ -0,0 +1,33 @@
+﻿/*Nút back to top*/
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+});
+
+/*Nút tìm kiếm trên di động*/
+$("#button-search").click(function () {
+    console.log($("#search-box").is(":visible"));
+    if (!$("#search-box").is(":visible")) {
+        $("#search-box").slideToggle("medium");
+        $("#dl-menu").css("border-bottom", "none");
+    } else {
+        $("#search-box").slideToggle("medium");
+        $("#dl-menu").css("border-bottom", "solid 2px rgb(0, 156, 255)");
+    }
+});
